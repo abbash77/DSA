6f01f1248b4823069a0f7bf10973d129fb5c43d3
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-08-19 12:59:08 +0530

    Time: 46 ms (16.26%), Space: 17.9 MB (93.42%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int a;
-        map<int,int> m;
-        for(int i=0;i<=nums.size();i++){
-            m[i]=0;
+        int n = nums.size();
+        int reqSum = (n*(n + 1))/2;
+        
+        int sum = 0;
+        
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]=1;
-        }
-        for(auto i:m){
-            if(i.second==0){
-                a=i.first;
-            }
-        }
-        return a;
+        
+        return reqSum - sum;
     }
 };
