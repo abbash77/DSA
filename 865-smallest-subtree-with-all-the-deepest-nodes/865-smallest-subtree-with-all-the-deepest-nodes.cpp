e4b1ce8915b50859d1e6016a8b8b4d7edbb7f0e2
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-10-01 21:31:36 +0530

    Time: 15 ms (16.27%), Space: 17.6 MB (5.52%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root,vector<int> &all,int level,int &maxLevel){
+        if(root==NULL){
+            return ;
+        }
+        if(root->left==NULL&&root->right==NULL){
+            if(level>maxLevel){
+                all.clear();
+                all.push_back(root->val);
+                maxLevel=level;
+            }
+            else if(level==maxLevel){
+                all.push_back(root->val);
+            }
+        }
+        helper(root->left,all,level+1,maxLevel);
+        helper(root->right,all,level+1,maxLevel);
+    }
+    TreeNode* help(TreeNode* root,vector<int> all){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->left==NULL&&root->right==NULL){
+            for(int i=0;i<all.size();i++){
+                if(root->val==all[i]){
+                    return root;
+                }
+            }
+            return NULL;
+        }
+        
+        TreeNode* left=help(root->left,all);
+        TreeNode* right=help(root->right,all);
+        if(left!=NULL&&right==NULL){
+            return left;
+        }
+        else if(left==NULL&&right!=NULL){
+            return right;
+        }
+        else if(left!=NULL&&right!=NULL){
+            return root;
+        }
+        else{
+            return NULL;
+        }
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int maxLevel=INT_MIN;
+        vector<int> all;
+        helper(root,all,0,maxLevel);
+        return help(root,all);
+    }
+};
