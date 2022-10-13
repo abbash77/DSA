c37ba12ca0356d6568a0540e867da82ed0403406
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-10-13 23:41:20 +0530

    Time: 47 ms (42.18%), Space: 13.6 MB (69.86%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+     bool checkCycle(vector<int> adj[], int i, vector<int> &vis, vector<int> &dfsvis){
+        vis[i] = 1;
+        dfsvis[i] = 1;
+        for(int u : adj[i]){
+            if(!vis[u]){
+                if(checkCycle(adj, u, vis, dfsvis)) return true;
+            }
+            else if(dfsvis[u]) return true;
+        }
+        dfsvis[i] = 0;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& pr) {
+        // Build graph
+        vector<int> adj[V];
+        for(int i = 0; i<pr.size(); ++i){
+            int u = pr[i][0];
+            int v = pr[i][1];
+            adj[v].push_back(u);
+        }
+        // check whether graph is DAG or not
+        vector<int> visited(V, 0);
+        vector<int> dfsvis(V, 0);
+        for(int i = 0; i<V; ++i){
+            if(!visited[i]){
+                if(checkCycle(adj, i, visited, dfsvis)) return false;
+            }
+        }
+        return true;
+    }
+};
