09b7864f0b9d289f7f81fd1f94b0614db4931afd
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-10-13 23:02:16 +0530

    Time: 343 ms (42.96%), Space: 46.8 MB (87.90%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& graph,int node,vector<bool> &visited,vector<bool> &dfsvisited){
+    bool dfs(vector<vector<int>>& graph,int node,vector<bool> &visited,vector<bool> &dfsvisited,vector<bool>& presentcycle){
         visited[node]=true;
         dfsvisited[node]=true;
         for(auto i:graph[node]){
             if(!visited[i]){
-                bool iscycle=dfs(graph,i,visited,dfsvisited);
+                bool iscycle=dfs(graph,i,visited,dfsvisited,presentcycle);
                 if(iscycle){
+                    presentcycle[node]=true;
                     return true;
                 }
                 
             }
             else if(dfsvisited[i]){
+                    presentcycle[node]=true;
                     return true;
                 }
             
@@ -22,15 +24,16 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<bool> visited(graph.size(),false);
         vector<bool> dfsvisited(graph.size(),false);
+        vector<bool> presentcycle(graph.size(),false);
         vector<int> ans;
         for(int i=0;i<graph.size();i++){
             if(!visited[i]){
-                dfs(graph,i,visited,dfsvisited);
+                dfs(graph,i,visited,dfsvisited,presentcycle);
             }
             
         }
         for(int i=0;i<graph.size();i++){
-            if(dfsvisited[i]==0)
+            if(presentcycle[i]==0)
                 ans.push_back(i);
         }
         return ans;
