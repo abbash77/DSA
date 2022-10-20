b0b92e20162622ac9e50baebfaf85c03db834e3e
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-10-21 00:58:15 +0530

    Time: 49 ms (15.67%), Space: 8.9 MB (14.76%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> m;
+        for(int i=0;i<magazine.length();i++){
+            m[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            if(m[ransomNote[i]]==0){
+                return false;
+            }
+            else{
+                m[ransomNote[i]]--;
+            }
+        }
+        return true;
+        
+    }
+};
