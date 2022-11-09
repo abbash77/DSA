18552912f7290e937e9cd85cd25b64561edaa25e
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-11-09 23:40:12 +0530

    Time: 7 ms (47.63%), Space: 7.7 MB (56.35%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -3,6 +3,7 @@ public:
     void helper(vector<int>& nums,vector<int>& help,vector<vector<int>> &ans,unordered_map<int,bool>& m){
         if(help.size()==nums.size()){
             ans.push_back(help);
+            return;
         }
         for(int i=0;i<nums.size();i++){
             if(!m[nums[i]]){
