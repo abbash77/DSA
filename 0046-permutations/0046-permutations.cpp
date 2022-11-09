b76e8e079d3074d4965be29d62c913335cb583fa
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-11-09 23:37:34 +0530

    Time: 8 ms (35.10%), Space: 7.8 MB (56.35%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void helper(vector<int>& nums,vector<int>& help,vector<vector<int>> &ans,unordered_map<int,bool>& m){
+        if(help.size()==nums.size()){
+            ans.push_back(help);
+        }
+        for(int i=0;i<nums.size();i++){
+            if(!m[nums[i]]){
+                help.push_back(nums[i]);
+                m[nums[i]]=true;
+                helper(nums,help,ans,m);
+                help.pop_back();
+                m[nums[i]]=false;
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> help;
+        unordered_map<int,bool> m;
+        helper(nums,help,ans,m);
+        return ans;
+    }
+};
