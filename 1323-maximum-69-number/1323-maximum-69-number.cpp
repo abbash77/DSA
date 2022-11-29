d32a74c2f1d92447665cce0c6d37a62964ad9420
abbash77 <87925220+abbash77@users.noreply.github.com>
2022-11-29 19:35:24 +0530

    Time: 3 ms (38.82%), Space: 6.2 MB (8.56%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> a;
-        while(num>0){
-            a.push_back(num%10);
-            num=num/10;
+       int rightDigCount = -1;
+        int digCount = 0; 
+        int temp = num; 
+        
+        while(temp > 0) 
+        {
+            int dig = temp % 10;
+            if (dig == 6) rightDigCount = digCount; 
+            
+            digCount++;
+            temp = temp / 10;
         }
-        reverse(a.begin(),a.end());
-        for(int i=0;i<a.size();i++){
-            if(a[i]==6){
-                a[i]=9;
-                break;
-            }
-        }
-        int ans=0;
-        for(int i=0;i<a.size();i++){
-            ans+=a[i]*pow(10,a.size()-i-1);
-        }
+        
+        if (rightDigCount == -1) return num;
+        int ans = num + (3 * pow(10, rightDigCount));
         return ans;
     }
 };
