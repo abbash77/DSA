1bf0b42eab8dc1427d5becf98e266c7230b6d57f
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-01-13 22:57:39 +0530

    Time: 5 ms (26.01%), Space: 6.2 MB (28.97%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,10 +1,26 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        for(int i=1;i<word.size();i++){
-            if( (isupper(word[1]) != isupper(word[i]))  ||  (islower(word[0]) && isupper(word[i])) )
-                return false;
+         int count=0;
+        if(word[0]>=97 && word[0]<123){
+            for(int i=1;i<word.length();i++){
+                if(word[i]>=65 && word[i]<91){
+                    return false;
+                }
+            }
         }
-        return true;
+        if(word[0]>=65 && word[0]<91){
+            for(int i=1;i<word.length();i++){
+                if(word[i]>=65 && word[i]<91){
+                    count++;
+                }
+            }
+        }
+        if(count==0 || count==word.length()-1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
