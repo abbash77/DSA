d158ded75290dc1549be882ea86cf65f8be2d8ca
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-04-24 23:33:47 +0530

    Time: 16 ms (59.74%), Space: 10.8 MB (38.96%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,45 +1,15 @@
 class Solution {
 public:
-    static bool cmp(vector<int> a,vector<int> b){
-        return a[1]<b[1];
-    }
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        sort(trips.begin(),trips.end(),cmp);
-        map<int,int> m;
-        // for(int i=0;i<trips.size();i++){
-        //     m[trips[i][2]]+=trips[i][0];
-        // }
-        int cap=capacity;
-        cap-=trips[0][0];
-        if(cap<0){
-            return false;
+        map<int, int> sortedmap;
+        for (int i = 0; i < trips.size(); ++i) {
+             sortedmap[trips[i][1]] += trips[i][0]; // picking up the passengers resulting in occupany in seats
+             sortedmap[trips[i][2]] -= trips[i][0]; //droping off the passengers resulting in reduction of filling seats
         }
-        int start=trips[0][1];
-        int end=trips[0][2];
-        m[trips[0][2]]=trips[0][0];
-        for(int i=1;i<trips.size();i++){
-            for(auto k:m){
-                if(k.first<=trips[i][1]){
-                    cap+=k.second;
-                    m.erase(k.first);
-                }
-                else{
-                    break;
-                }
-            }
-            if(start<trips[i][1]<end){
-                cap-=trips[i][0];
-                if(cap<0){
-                    return false;
-                }
-                end=trips[i][2];
-                m[trips[i][2]]+=trips[i][0];
-            }else{
-                cap=capacity;
-                start=trips[i][1];
-                end=trips[i][2];
-                m.clear();
-            }
+        for (auto it = sortedmap.begin(); it != sortedmap.end();it++) { 
+            capacity -= it->second;
+            
+            if (capacity < 0) return false;
         }
         return true;
     }
