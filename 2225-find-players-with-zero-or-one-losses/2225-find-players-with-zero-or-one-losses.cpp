540dce3cdd027504311a7955f4845ffe2a21399e
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-06-20 13:59:47 +0530

    Time: 654 ms (80.10%), Space: 161 MB (95.70%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -9,22 +9,18 @@ public:
         for(int i=0;i<matches.size();i++){
             m[matches[i][1]]++;
         }
-        vector<vector<int>> ans;
-        vector<int> v1;
-        vector<int> v2;
+        vector<vector<int>> ans(2);
         for(auto i:m){
             if(i.second==0){
-                v1.push_back(i.first);
+                ans[0].push_back(i.first);
             }
             if(i.second==1){
-                v2.push_back(i.first);
+                ans[1].push_back(i.first);
             }
             
         }
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-        ans.push_back(v1);
-        ans.push_back(v2);
+        sort(ans[0].begin(),ans[0].end());
+        sort(ans[1].begin(),ans[1].end());
         return ans;
         
     }
