3d09e37347ddb211b47effa18b5e4adf54b787ca
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-06-23 23:06:33 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.79%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -1,56 +1,15 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        unordered_map<string,int> m;
-        int j=0;
+        string s=to_string(n);
+        sort(s.begin(),s.end());
         
-        long long r=n;
-        string main="";
-         while(r>0){
-                int a=r%10;
-                main+=a+'0';
-                r=r/10;
+        for(int i=0;i<31;i++){
+            int n=pow(2,i);
+            string x=to_string(n);
+            sort(x.begin(),x.end());
+            if(x==s) return true;
         }
-         reverse(main.begin(),main.end());
-        string s="";
-        while(s.length()<=main.length()){
-            s="";
-            long long l=pow(2,j);
-            while(l>0){
-                long long a=l%10;
-                s+=a+'0';
-                l=l/10;
-            }
-            reverse(s.begin(),s.end());
-            m[s]=s.length();
-            j++;
-        }
-        vector<string> v;
-        for(auto i:m){
-            if(i.second==main.length()){
-                v.push_back(i.first);
-            }
-        }
-        unordered_map<char,int> m1;
-        for(int i=0;i<main.length();i++){
-            m1[main[i]]++;
-        }
-        
-        unordered_map<char,int> m2=m1;
-        for(int i=0;i<v.size();i++){
-            int p=0;
-            for(int j=0;j<v[i].size();j++){
-                m2[v[i][j]]--;
-                if(m2[v[i][j]]==0){
-                    p++;
-                }
-            }
-            if(p==m2.size()){
-                return true;
-            }
-            m2=m1;
-            
-        }
-        return false;
+         return false;
     }
 };
