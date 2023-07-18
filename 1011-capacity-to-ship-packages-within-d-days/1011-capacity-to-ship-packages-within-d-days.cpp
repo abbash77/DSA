05536c53cc10291096d0ca2ea6873dfe9832c9a0
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-07-18 18:27:00 +0530

    Time: 109 ms (21.16%), Space: 56.3 MB (8.11%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isPossible(int mid,vector<int> nums,int m){
+        int sum=0;
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(sum+nums[i]<=mid){
+                sum+=nums[i];
+            }
+            else{
+                sum=nums[i];
+                k++;
+                if(k==m||nums[i]>mid){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    int shipWithinDays(vector<int>& nums, int m) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            
+        }
+        int s=0;
+        int e=sum;
+        int ans;
+        int mid=(s+e)/2;
+        while(s<=e){
+            if(isPossible(mid,nums,m)){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            mid=(s+e)/2;
+        }
+        return ans;
+    }
+};
