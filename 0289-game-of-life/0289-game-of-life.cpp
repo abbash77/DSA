5a16477feb48325b69a7e29b77fc2ad8a0b35800
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-09-06 21:47:14 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (77.13%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.cpp
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -4,60 +4,64 @@ public:
         int n=board.size();
         int m=board[0].size();
         int cnt=0;
-        if(i-1>=0&&board[i-1][j]==1){
+        if(i-1>=0&&(board[i-1][j]==1||board[i-1][j]==-1)){
             cnt++;
         }
-        if(j-1>=0&&board[i][j-1]==1){
+        if(j-1>=0&&(board[i][j-1]==1||board[i][j-1]==-1)){
             cnt++;
         }
-        if(i+1<n&&board[i+1][j]==1){
+        if(i+1<n&&(board[i+1][j]==1||board[i+1][j]==-1)){
             cnt++;
         }
-        if(j+1<m&&board[i][j+1]==1){
+        if(j+1<m&&(board[i][j+1]==1||board[i][j+1]==-1)){
             cnt++;
         }
-        if(i-1>=0&&j-1>=0&&board[i-1][j-1]==1){
+        if(i-1>=0&&j-1>=0&&(board[i-1][j-1]==1||board[i-1][j-1]==-1)){
             cnt++;
         }
-        if(i-1>=0&&j+1<m&&board[i-1][j+1]==1){
+        if(i-1>=0&&j+1<m&&(board[i-1][j+1]==1||board[i-1][j+1]==-1)){
             cnt++;
         }
-        if(i+1<n&&j-1>=0&&board[i+1][j-1]==1){
+        if(i+1<n&&j-1>=0&&(board[i+1][j-1]==1||board[i+1][j-1]==-1)){
             cnt++;
         }
-        if(i+1<n&&j+1<m&&board[i+1][j+1]==1){
+        if(i+1<n&&j+1<m&&(board[i+1][j+1]==1||board[i+1][j+1]==-1)){
             cnt++;
         }
-        // cout<<cnt<<endl;
+        cout<<cnt<<endl;
         return cnt;
     }
     void gameOfLife(vector<vector<int>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<int>> ans(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 int totalNeigh=coutNeighbour(i,j,board);
                 if(board[i][j]==0&&totalNeigh==3){
-                    ans[i][j]=1;
+                    board[i][j]=2;
                 }
                 else if(board[i][j]==1){
                     if(totalNeigh==1||totalNeigh==0){
                         
-                        ans[i][j]=0;
+                        board[i][j]=-1;
                     }
                     else if(totalNeigh==2||totalNeigh==3){
-                        ans[i][j]=1;
+                        board[i][j]=1;
                     }
                     else if(totalNeigh>=4){
-                        ans[i][j]=0;
+                        board[i][j]=-1;
                     }
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                board[i][j]=ans[i][j];
+                if(board[i][j]==-1){
+                    board[i][j]=0;
+                }
+                else if(board[i][j]==2){
+                    board[i][j]=1;
+                }
             }
         }
         
