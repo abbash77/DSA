59c76ad5f8726451ce766242d478225618fd66b0
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-10-27 21:49:59 +0530

    Time: 158 ms (83.15%), Space: 102.4 MB (53.54%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        unordered_map<char,int> m;
-        int ans=0;
-        for(int i=garbage.size()-1;i>=0;i--){
-            for(int j=0;j<garbage[i].size();j++){
-                if(m.find('G')==m.end()&&garbage[i][j]=='G'){
-                    m['G']=i;
+        int sum=0;
+       // travel[-1]=0;
+        sum+=garbage[0].size();
+        int sum2=0;int cntg=0;int cntp=0;int cntm=0;
+        for(int i=1;i<garbage.size();i++){
+            sum2+=travel[i-1];
+            for(auto it:garbage[i]){
+                if(it=='G'){
+                    cntg=sum2;
+                     
                 }
-                else if(m.find('M')==m.end()&&garbage[i][j]=='M'){
-                    m['M']=i;
+                else if(it=='M'){
+                   cntm=sum2;
+                   
                 }
-                else if(m.find('P')==m.end()&&garbage[i][j]=='P'){
-                    m['P']=i;
+                else{
+                   cntp=sum2;
+                   
                 }
+
+                sum++;
+               
             }
-            if(m.size()==3){
-                break;
-            }
-        }
-        vector<pair<string,unordered_map<char,int>>> v;
-        for(int i=0;i<garbage.size();i++){
-            unordered_map<char,int> temp;
-            for(int j=0;j<garbage[i].size();j++){
-                temp[garbage[i][j]]++;
-            }
-            v.push_back({garbage[i],temp});
-        }
-        if(m.find('G')!=m.end()){
-            int idx=m['G'];
             
-            for(int i=0;i<=idx;i++){
-                ans+=v[i].second['G'];
-                if(i!=idx){
-                    ans+=travel[i];
-                }
-            }
         }
-       
-        
-        if(m.find('M')!=m.end()){
-            int idx=m['M'];
-            
-            for(int i=0;i<=idx;i++){
-                ans+=v[i].second['M'];
-                if(i!=idx){
-                    ans+=travel[i];
-                }
-            }
-        }
-        if(m.find('P')!=m.end()){
-            int idx=m['P'];
-           
-            for(int i=0;i<=idx;i++){
-                ans+=v[i].second['P'];
-                if(i!=idx){
-                    ans+=travel[i];
-                }
-            }
-        }
-        return ans;
+        sum+=(cntp+cntg+cntm);
+     
+        return sum;
     }
 };
