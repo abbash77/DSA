cf967d53c437eeba79f3b07ebb1d51f7966416e4
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-10-27 21:43:38 +0530

    Time: 797 ms (5.07%), Space: 389.8 MB (5.21%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,62 +1,64 @@
 class Solution {
 public:
-    bool present(char x,string str){
-       for(int i=0;i<str.length();i++){
-           if(str[i]==x){
-               return true;
-           }
-       }
-        return false;
-    }
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int sum=0;
-        for(int i=0;i<travel.size();i++){
-            sum+=travel[i];
+        unordered_map<char,int> m;
+        int ans=0;
+        for(int i=garbage.size()-1;i>=0;i--){
+            for(int j=0;j<garbage[i].size();j++){
+                if(m.find('G')==m.end()&&garbage[i][j]=='G'){
+                    m['G']=i;
+                }
+                else if(m.find('M')==m.end()&&garbage[i][j]=='M'){
+                    m['M']=i;
+                }
+                else if(m.find('P')==m.end()&&garbage[i][j]=='P'){
+                    m['P']=i;
+                }
+            }
+            if(m.size()==3){
+                break;
+            }
         }
-        int count=0;
+        vector<pair<string,unordered_map<char,int>>> v;
         for(int i=0;i<garbage.size();i++){
-            count+=garbage[i].length();
-        }
-        int a=sum;
-        for(int i=garbage.size()-1;i>=0;i--){
-            if(present('G',garbage[i])){
-                break;
+            unordered_map<char,int> temp;
+            for(int j=0;j<garbage[i].size();j++){
+                temp[garbage[i][j]]++;
             }
-            else{
-                if(i>0){
-                    a-=travel[i-1];
+            v.push_back({garbage[i],temp});
+        }
+        if(m.find('G')!=m.end()){
+            int idx=m['G'];
+            
+            for(int i=0;i<=idx;i++){
+                ans+=v[i].second['G'];
+                if(i!=idx){
+                    ans+=travel[i];
                 }
-                
             }
         }
-        cout<<a<<" ";
-        int b=sum;
-        for(int i=garbage.size()-1;i>=0;i--){
-            if(present('P',garbage[i])){
-                break;
-            }
-            else{
-                if(i>0){
-                    b-=travel[i-1];
-                }
-                
-            }
-        }
-        cout<<b<<" ";
-        int c=sum;
-         for(int i=garbage.size()-1;i>=0;i--){
-            if(present('M',garbage[i])){
-                break;
-            }
-            else{
-                if(i>0){
-                    c-=travel[i-1];
-                }
-                
-            }
-        }
-        cout<<c<<" ";
-        return a+b+c+count;
+       
         
+        if(m.find('M')!=m.end()){
+            int idx=m['M'];
+            
+            for(int i=0;i<=idx;i++){
+                ans+=v[i].second['M'];
+                if(i!=idx){
+                    ans+=travel[i];
+                }
+            }
+        }
+        if(m.find('P')!=m.end()){
+            int idx=m['P'];
+           
+            for(int i=0;i<=idx;i++){
+                ans+=v[i].second['P'];
+                if(i!=idx){
+                    ans+=travel[i];
+                }
+            }
+        }
+        return ans;
     }
 };
