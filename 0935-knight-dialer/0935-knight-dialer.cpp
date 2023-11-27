8ca56abe3d2a10b498f2568b17b2c1843b91ccfe
abbash77 <87925220+abbash77@users.noreply.github.com>
2023-11-28 00:39:16 +0530

    Time: 185 ms (33.63%), Space: 38.2 MB (38.16%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    int M = 1e9+7;
-    vector<vector<int>> adj = {
-        {4, 6},
-        {6, 8},
-        {7, 9},
-        {4, 8},
-        {3, 9, 0},
-        {},
-        {1, 7, 0},
-        {2, 6},
-        {1, 3},
-        {2, 4}
-    };
-    int solve(int n, int cell, vector<vector<int>> &dp){
-        if(n == 0)  return 1;
-        if(dp[cell][n] != -1)   return dp[cell][n];
-        int ans = 0;
-        for(int nextCell: adj[cell]){
-            ans = (ans + solve(n-1, nextCell, dp)) % M;
+    int mod=1e9+7;
+    int help(int i,int n,vector<vector<int>>&v,vector<vector<int>>&dp)
+    {
+        if(n==0)
+        {
+            return 1;
         }
-        return dp[cell][n] = ans;
+        if(dp[n][i]!=-1)
+        {
+            return dp[n][i];
+        }
+        int ans=0;
+        
+        for(int j=0;j<v[i].size();j++)
+        {
+            ans =(ans + help(v[i][j],n-1,v,dp))%mod;
+        }
+          
+        
+        return dp[n][i]=ans;
     }
     int knightDialer(int n) {
-        int result = 0;
-        vector<vector<int>> dp(10, vector<int> (n+1, -1));
-        for(int cell=0; cell<=9; cell++){
-            result = (result + solve(n-1, cell, dp) % M)%M;
+        vector<vector<int>>v={{4,6},{6,8},{7,9},{4,8},{3,9,0},{},{1,7,0},{2,6},{1,3},{2,4}};
+        vector<vector<int>>dp(n+1,vector<int>(10,-1));
+        int ans=0;
+        
+        for(int i=0;i<10;i++)
+        {
+            ans=(ans+help(i,n-1,v,dp))%mod;
         }
-        return result;
+        
+        return ans;
     }
 };
