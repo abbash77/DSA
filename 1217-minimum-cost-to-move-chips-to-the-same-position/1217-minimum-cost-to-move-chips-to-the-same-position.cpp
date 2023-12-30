18d426adb0c3fd293c9bb96a5e15331262d27358
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2023-12-30 11:53:24 +0530

    Time: 4 ms (28.83%), Space: 7.7 MB (23.59%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minCostToMoveChips(vector<int>& position) {
-        unordered_map<int,int> m;
         int evenVal=0;
         int oddVal=0;
         for(int i=0;i<position.size();i++){
-            m[position[i]]++;
-        }
-        for(auto i:m){
-            if(i.first%2==0){
-                evenVal+=i.second;
+            if(position[i]%2==0){
+                evenVal++;
             }
             else{
-                oddVal+=i.second;
+                oddVal++;
             }
         }
-        return min(evenVal,oddVal);
+        return min(oddVal,evenVal);
         
         
     }
