ce19a491a50b99465350f99c222cbd0577ea0266
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2023-12-30 11:12:33 +0530

    Time: 347 ms (5.20%), Space: 10.4 MB (6.57%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        vector<int> v;
+        vector<int> ans;
+        for(int i=0;i<boxes.size();i++){
+            if(boxes[i]=='1'){
+                v.push_back(i);
+            }
+        }
+        for(int i=0;i<boxes.size();i++){
+            int sum=0;
+            for(int j=0;j<v.size();j++){
+                sum+=abs(i-v[j]);
+            }
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
