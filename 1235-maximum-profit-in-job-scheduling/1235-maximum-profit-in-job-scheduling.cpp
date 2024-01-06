55f9e497f40f2e55420049fe6fdbc9ed86357747
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-01-06 18:04:41 +0530

    Time: 132 ms (79.30%), Space: 71.2 MB (71.73%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(vector<pair<int, pair<int, int>>> &v, vector<int>& s, int i){
+        if(i>=v.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int t=0, nt=0;
+        int ind=lower_bound(s.begin(), s.end(), v[i].second.first)-s.begin();
+        t=v[i].second.second+solve(v, s, ind);
+        nt=solve(v, s, i+1);
+        return dp[i]=max(t, nt);
+    }
+
+    int jobScheduling(vector<int>& s, vector<int>& e, vector<int>& p) {
+        vector<pair<int, pair<int, int>>> v;
+        int n=s.size();
+        dp.resize(n,  -1);
+        for(int i=0; i<n; i++){
+            v.push_back({s[i], {e[i], p[i]}});
+        }
+        sort(s.begin(), s.end());
+        sort(v.begin(), v.end());
+        return solve(v, s, 0);
+    }
+
+};
