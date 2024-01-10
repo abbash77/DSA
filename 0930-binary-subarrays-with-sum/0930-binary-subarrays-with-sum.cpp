3bebc9acd0d856bb64a49e26994ec914f09b61c8
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-01-10 12:49:58 +0530

    Time: 19 ms (93.60%), Space: 31 MB (57.22%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,58 +1,15 @@
 class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int i=0;
-        int sum=0;
-        int n=nums.size();
-        vector<int> d(n+1,1);
-        int v=1;
-        for(int j=n-1;j>=0;j--){
-            if(nums[j]==0){
-                v++;
-                d[j]=v;
-            }
-            else{
-                v=1;
-            }
-        }
-        int ans=0;
-        int m=0;
-        for(int j=0;j<nums.size();j++){
-            if(m==1){
-                sum=0;
-                i=j;
-                m=0;
-            }
-            sum+=nums[j];
-            if(sum>goal){
-                
-                m=1;
-                
-            }
-            else if(sum==goal){
-                ans+=d[j+1];
-                int a=0;
-                while(i<=j){
-                    if(nums[i]==1){
-                        i++;
-                        sum--;
-                        if(a!=0){
-                            ans+=d[j+1];
-                        }
-                        a++;
-                        
-                        break;
-                    }
-                    if(a!=0){
-                        ans+=d[j+1];
-                        
-                    }
-                    a++;
-                    i++;
-
-                }
-            }
+    int atMost(vector<int> A, int goal) {
+        int N = A.size(), i = 0, j = 0, cnt = 0, ans = 0;
+        while (j < N) {
+            cnt += A[j++];
+            while (i < j && cnt > goal) cnt -= A[i++];
+            ans += j - i+1;
         }
         return ans;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& A, int goal) {
+        return atMost(A, goal) - atMost(A, goal - 1);
     }
 };
