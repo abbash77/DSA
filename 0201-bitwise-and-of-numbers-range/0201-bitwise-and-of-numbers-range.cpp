119cec1df8c747c4c33f822d855043a24152672d
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-02-21 22:55:42 +0530

    Time: 102 ms (5.16%), Space: 8.8 MB (46.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int rangeBitwiseAnd(int l, int r) {
-        int s=0;
-       while(l<r){
-           l>>=1;  //l left
-           r>>=1;  //r right
-           s++;
-       }
-       return l<<s;
+    int rangeBitwiseAnd(int left, int right) {
+        
+        for(int i=0;i<32;i++){
+            long long val=pow(2,i);
+            if(left<val&&val<right){
+                return 0;
+            }
+        }
+        long long ans=left;
+        if(left<right){
+            for(long long i=left+1;i<=right;i++){
+                ans=ans&i;
+            }
+        }
+        
+        return ans;
         
     }
 };
