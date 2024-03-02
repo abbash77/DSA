38f0a74ae59ed93273b2ea5d0f4cbe7e66507619
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-03-02 19:51:59 +0530

    Time: 20 ms (78.12%), Space: 28.5 MB (46.74%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,50 +1,25 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int miniIndex=nums.size()-1;
-        int miniVal=INT_MAX;
-        // for(int i=0;i<nums.size();i++){
-        //     if(0<=nums[i]){
-        //         miniIndex=i;
-        //         break;
-        //     }
-        // }
-        for(int i=0;i<nums.size();i++){
-            if(miniVal>abs(nums[i])){
-                miniIndex=i;
-                miniVal=abs(nums[i]);
+        int n = nums.size();
+        int start=0;
+        int end=n-1;
+        
+        vector <int> res(n);
+        int pos = n-1;
+        
+        while(start <= end)
+        {
+            if(abs(nums[start]) < abs(nums[end])) {
+                res[pos--] = nums[end] * nums[end];
+                end--;
+            } else{
+                res[pos--] = nums[start] * nums[start];
+                start++;
             }
         }
-        vector<int> ans;
-        ans.push_back(nums[miniIndex]*nums[miniIndex]);
-        int i=miniIndex-1;
-        int j=miniIndex+1;
-       
-        while(i>=0&&j<nums.size()){
-            if(abs(nums[i])<abs(nums[j])){
-                ans.push_back(nums[i]*nums[i]);
-                i--;
-            }
-            else if(abs(nums[i])>abs(nums[j])){
-                ans.push_back(nums[j]*nums[j]);
-                j++;
-            }
-            else {
-                ans.push_back(nums[i]*nums[i]);
-                ans.push_back(nums[j]*nums[j]);
-                i--;
-                j++;
-            }
-        }
-        while(i>=0){
-            ans.push_back(nums[i]*nums[i]);
-            i--;
-        }
-        while(j<nums.size()){
-            ans.push_back(nums[j]*nums[j]);
-            j++;
-        }
-        return ans;
+        
+        return res;
         
     }
 };
