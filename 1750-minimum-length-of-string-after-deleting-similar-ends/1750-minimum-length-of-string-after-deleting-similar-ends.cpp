6197aed0fca065c795d7e690cd388dcaae9c2708
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-03-05 10:45:36 +0530

    Time: 26 ms (55.86%), Space: 14.1 MB (5.86%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        //2 1 1 2 1 1 2 1 1
-        int n=s.length();
-        vector<int> suffix(n,1);
-        for(int i=n-2;i>=0;i--){
-            if(s[i+1]==s[i]){
-                suffix[i]=suffix[i+1]+1;
-            }
-        }
-        vector<int> prefix(n,1);
-        for(int i=1;i<n;i++){
-            if(s[i-1]==s[i]){
-                prefix[i]=prefix[i-1]+1;
-            }
-        }
-        
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(s[i]==s[j]){
-                i+=suffix[i];
-                j-=prefix[j];
-            }
-            else{
-                break;
-            }
-        }
-        if(j-i<0){
-            return 0;
-        }
-        return j-i+1;
+        int i = 0, j = s.size() - 1;
+    while (i < j && s[i] == s[j]) {
+        auto ch = s[i];
+        while (i <= j && s[i] == ch)
+            ++i;
+        while (i < j && s[j] == ch)
+            --j;
+    }
+    return j - i + 1;
         
     }
 };
