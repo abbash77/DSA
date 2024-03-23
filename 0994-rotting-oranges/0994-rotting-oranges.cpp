17575e8f8a9f20e13d8e4a43f630483f3a3068b5
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-03-23 17:09:07 +0530

    Time: 7 ms (42.72%), Space: 16.1 MB (42.95%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,46 +2,59 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
-            }
-        }
-        q.push({-1,-1});
-        int ans=0;
-        while(!q.empty()){
-            auto front=q.front();
-            q.pop();
-            int i=front.first;
-            int j=front.second;
-            if(i==-1){
-                ans+=1;
-                if(!q.empty()){
-                    q.push({-1,-1});
-                }
-                
-                continue;
-            }
-            if(i-1>=0&&grid[i-1][j]==1){
-                grid[i-1][j]=2;
-                q.push({i-1,j});
-            }
-            if(i+1<grid.size()&&grid[i+1][j]==1){
-                grid[i+1][j]=2;
-                q.push({i+1,j});
-            }
-            if(j-1>=0&&grid[i][j-1]==1){
-                grid[i][j-1]=2;
-                q.push({i,j-1});
-            }
-            if(j+1<grid[0].size()&&grid[i][j+1]==1){
-                grid[i][j+1]=2;
-                q.push({i,j+1});
-            }
+        vector<vector<int>>visited(grid.size(),vector<int>(grid[0].size(),0));
+        int flag=0;
+      for(int i=0;i<grid.size();i++){
+          for(int j=0;j<grid[0].size();j++){
+              if(grid[i][j]==2){
+                q.push({i,j});
+                visited[i][j]=true;
+              }
+              if(grid[i][j]==1){
+                  flag=1;
+              }
             
+          }
+      }
+        if(flag==0){
+            return 0;
         }
+        int time=0;
+      while(!q.empty()){
+          int n=q.size();
+          for(int i=0;i<n;i++){
+               pair<int,int> top=q.front();
+               q.pop();
+              
+               int a=top.first;
+               int b=top.second;
+              //visited ,1,boundry
+              
+               if(a-1>=0 && grid[a-1][b]==1 && !visited[a-1][b]){
+                   grid[a-1][b]=2;
+                   visited[a-1][b]=true;
+                  q.push({a-1,b});
+               }
+              if(b-1>=0 && grid[a][b-1]==1 && !visited[a][b-1]){
+                  visited[a][b-1]=true;
+                  q.push({a,b-1});
+                   grid[a][b-1]=2;
+              }
+              if(a+1<grid.size() && grid[a+1][b]==1 && !visited[a+1][b]){
+                  visited[a+1][b]=true;
+                  q.push({a+1,b});
+                   grid[a+1][b]=2;
+              }
+              if(b+1<grid[0].size()&& grid[a][b+1]==1 && !visited[a][b+1]){
+                  visited[a][b+1]=true;
+                  q.push({a,b+1});
+                   grid[a][b+1]=2;
+              }
+              
+          }
+          time++;
+         
+      }
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==1){
@@ -49,6 +62,6 @@ public:
                 }
             }
         }
-        return ans-1;
+        return time-1;
     }
 };
