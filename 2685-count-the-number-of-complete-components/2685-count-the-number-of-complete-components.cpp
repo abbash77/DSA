709e169e58796d4390d29e67008847f9b75e0257
Abbas Haider <87925220+abbash77@users.noreply.github.com>
2024-03-30 13:50:09 +0530

    Time: 143 ms (81.55%), Space: 115 MB (97.92%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,38 +1,73 @@
+class DSU{
+    public:
+    int n;
+    vector<int> parent;
+    vector<int> size;
+    ////////////////////////////as ques demands
+    vector<int> edg;
+    DSU(int d){
+        n=d;
+        parent.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+        size.resize(n,1);
+        edg.resize(n,0);
+    }
+    int findParent(int a){
+        if(parent[a]==a){
+            return a;
+        }
+        return parent[a]=findParent(parent[a]);
+    }
+    void unionBySize(int a,int b){
+        int para=findParent(a);
+        int parb=findParent(b);
+        if(para==parb){
+            //bcz yahan dsu se matlb nhi hai yahan seedha for loop se dikh rsaha ki ek edge hai to humin badhana hai
+            //size==no of vertices
+            //n(n-1)/2 total max edges formuale 
+            //we just have to check that
+            edg[para]++;
+            return;
+        }
+        else if(size[para]==size[parb]){
+            parent[parb]=para;
+            size[para]+=size[parb];
+            edg[para]++;
+        }
+        else if(size[para]>size[parb]){
+            parent[parb]=para;
+            size[para]+=size[parb];
+            edg[para]++;
+        }
+        else{
+            parent[para]=parb;
+            size[parb]+=size[para];
+            edg[parb]++;
+        }
+    }
+
+    
+};
+
 class Solution {
 public:
-    void get(int s,vector<vector<int>> &g,int &y,vector<bool> &v){
-        if(v[s])return;
-        v[s] = true;
-        y++;
-        for(auto &i: g[s]){
-            get(i,g,y,v);
-        }
-    }
-    void solve(int s,vector<vector<int>> &g,int &x,int &y,vector<bool> &v){
-        if(v[s])return;
-        v[s] = true;
-        for(auto &i: g[s]){
-            if(g[i].size() != y)x = 0;
-            solve(i,g,x,y,v);
-        }
-    }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> g(n);
-        vector<bool> v1(n,false),v2(n,false);
-        for(auto &i: edges){
-            g[i[0]].push_back(i[1]);
-            g[i[1]].push_back(i[0]);
+        DSU d(n);
+        for(int i=0;i<edges.size();i++){
+            d.unionBySize(edges[i][0],edges[i][1]);
         }
-        int y = 0,x=0,ans=0;
-        for(int i = 0; i < n; i++){
-            if(!v1[i]){
-                y = -1;
-                get(i,g,y,v1);
-                x = 1;
-                solve(i,g,x,y,v2);
-                ans += x;
+        int count=0;
+        for(int i=0;i<d.parent.size();i++){
+            if(d.parent[i]==i){
+                int m=d.size[i];
+                int x=(m*(m-1))/2;
+                if(d.edg[i]==x){
+                    count++;
+                }
             }
         }
-        return ans;
+        return count;
     }
 };
